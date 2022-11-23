485031dedc8cb917d020d980d5b511441679e421
Sm0keF1nger <kkaptelov@gmail.com>
2022-11-23 22:40:04 +0200

    first commit

---
+++ lab4_z2/lab4_z2.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int i;
+    i = 20;
+    double sum;
+    sum = 1;
+    
+    for (i = 20; i <= 80; i += 5) {
+        sum *= i;
+    }
+    printf_s("20*25*30*35...*75*80 = %.lf\n", sum);
+    
+
+    
+    while (i < 81) {
+        sum *= i;
+        i += 5;
+    }
+    printf_s("20*25*30*35...*75*80 = %.lf\n", sum);
+    
+    
+    i = 20;
+    sum = 1;
+    do {
+        sum *= i;
+        i += 5;
+    } while (i < 81);
+    printf_s("20*25*30*35...*75*80 = %.lf\n", sum);
+}
+
+
+
